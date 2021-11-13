5e6660b71cc1b04eb5d9e5cc23b16a80eed0b167
Tim Pogue <t.pogue.python@gmail.com>
2021-11-13 13:41:29 -0600

    update to use poetry

--- bottle_pymysql.py
+++ bottle_pymysql.py
@@ -1,4 +1,4 @@
-'''
+"""
 Bottle-MySQL is a plugin that integrates MySQL with your Bottle
 application. It automatically connects to a database at the beginning of a
 request, passes the database handle to the route callback and closes the
@@ -28,22 +28,23 @@ Usage Example::
         if row:
             return template('showitem', page=row)
         return HTTPError(404, "Page not found")
-'''
+"""
 
 __author__ = "Alexandr N. Zamaraev"
-__version__ = '0.1.3'
-__license__ = 'MIT'
+__version__ = "0.1.4"
+__license__ = "MIT"
 
-### CUT HERE (see setup.py)
+# CUT HERE (see setup.py)
 
 import inspect
+
+import bottle
 import pymysql
 import pymysql.cursors as cursors
-import bottle
-
 
 # PluginError is defined to bottle >= 0.10
-if not hasattr(bottle, 'PluginError'):
+if not hasattr(bottle, "PluginError"):
+
     class PluginError(bottle.BottleException):
         pass
 
@@ -51,21 +52,31 @@ if not hasattr(bottle, 'PluginError'):
 
 
 class PyMySQLPlugin(object):
-    '''
+    """
     This plugin passes a mysql database handle to route callbacks
     that accept a `pymydb` keyword argument. If a callback does not expect
     such a parameter, no connection is made. You can override the database
     settings on a per-route basis.
-    '''
+    """
 
-    name = 'pymysql'
+    name = "pymysql"
     api = 2
 
     def __init__(
-        self, dbuser=None, dbpass=None, dbname=None, dbhost='localhost',
-        dbport=3306, dbunixsocket=None, autocommit=True, dictrows=True,
-        dbread_timeout=None, dbwrite_timeout=None,
-        keyword='pymydb', charset='utf8', timezone=None
+        self,
+        dbuser=None,
+        dbpass=None,
+        dbname=None,
+        dbhost="localhost",
+        dbport=3306,
+        dbunixsocket=None,
+        autocommit=True,
+        dictrows=True,
+        dbread_timeout=None,
+        dbwrite_timeout=None,
+        keyword="pymydb",
+        charset="utf8",
+        timezone=None,
     ):
         self.dbhost = dbhost
         self.dbport = dbport
@@ -82,23 +93,24 @@ class PyMySQLPlugin(object):
         self.timezone = timezone
 
     def setup(self, app):
-        '''
+        """
         Make sure that other installed plugins don't affect the same keyword argument.
-        '''
+        """
         for other in app.plugins:
             if not isinstance(other, PyMySQLPlugin):
                 continue
             if other.keyword == self.keyword:
                 raise PluginError(
-                    "Found another pymysql plugin with conflicting settings (non-unique keyword).")
+                    "Found another pymysql plugin with conflicting settings (non-unique keyword)."
+                )
             elif other.name == self.name:
-                self.name += '_%s' % self.keyword
+                self.name += "_%s" % self.keyword
 
     def apply(self, callback, route):
         # hack to support bottle v0.9.x
-        if bottle.__version__.startswith('0.9'):
-            config = route['config']
-            _callback = route['callback']
+        if bottle.__version__.startswith("0.9"):
+            config = route["config"]
+            _callback = route["callback"]
         else:
             config = route.config
             _callback = route.callback
@@ -106,29 +118,31 @@ class PyMySQLPlugin(object):
         # Override global configuration with route-specific values.
         if "pymysql" in config:
             # support for configuration before `ConfigDict` namespaces
-            g = lambda key, default: config.get('pymysql', {}).get(key, default)
+            g = lambda key, default: config.get("pymysql", {}).get(
+                key, default
+            )
         else:
-            g = lambda key, default: config.get('pymysql.' + key, default)
+            g = lambda key, default: config.get("pymysql." + key, default)
 
-        keyword = g('keyword', self.keyword)
+        keyword = g("keyword", self.keyword)
         # Test if the original callback accepts a 'pymydb' keyword.
         # Ignore it if it does not need a database handle.
         _args = inspect.getargspec(_callback)
         if keyword not in _args.args:
             return callback
 
-        dbhost = g('dbhost', self.dbhost)
-        dbport = g('dbport', self.dbport)
-        dbunixsocket = g('dbunixsocket', self.dbunixsocket)
-        dbuser = g('dbuser', self.dbuser)
-        dbpass = g('dbpass', self.dbpass)
-        dbname = g('dbname', self.dbname)
-        dbread_timeout = g('dbread_timeout', self.dbread_timeout)
-        dbwrite_timeout = g('dbwrite_timeout', self.dbwrite_timeout)
-        autocommit = g('autocommit', self.autocommit)
-        dictrows = g('dictrows', self.dictrows)
-        charset = g('charset', self.charset)
-        timezone = g('timezone', self.timezone)
+        dbhost = g("dbhost", self.dbhost)
+        dbport = g("dbport", self.dbport)
+        dbunixsocket = g("dbunixsocket", self.dbunixsocket)
+        dbuser = g("dbuser", self.dbuser)
+        dbpass = g("dbpass", self.dbpass)
+        dbname = g("dbname", self.dbname)
+        dbread_timeout = g("dbread_timeout", self.dbread_timeout)
+        dbwrite_timeout = g("dbwrite_timeout", self.dbwrite_timeout)
+        autocommit = g("autocommit", self.autocommit)
+        dictrows = g("dictrows", self.dictrows)
+        charset = g("charset", self.charset)
+        timezone = g("timezone", self.timezone)
 
         def wrapper(*args, **kwargs):
             # Connect to the database
@@ -136,33 +150,33 @@ class PyMySQLPlugin(object):
             try:
 
                 kw = {
-                    'user': dbuser,
-                    'passwd': dbpass,
-                    'db': dbname,
-                    'charset': charset,
+                    "user": dbuser,
+                    "passwd": dbpass,
+                    "db": dbname,
+                    "charset": charset,
                 }
 
                 if dictrows:
-                    kw['cursorclass'] = cursors.DictCursor
+                    kw["cursorclass"] = cursors.DictCursor
 
                 if dbunixsocket:
-                    kw['unix_socket'] = dbunixsocket
+                    kw["unix_socket"] = dbunixsocket
                 else:
-                    kw['host'] = dbhost
-                    kw['port'] = dbport
+                    kw["host"] = dbhost
+                    kw["port"] = dbport
 
                 if dbread_timeout is not None:
-                    kw['read_timeout'] = int(dbread_timeout)
+                    kw["read_timeout"] = int(dbread_timeout)
 
                 if dbwrite_timeout is not None:
-                    kw['write_timeout'] = int(dbwrite_timeout)
+                    kw["write_timeout"] = int(dbwrite_timeout)
 
                 con = pymysql.connect(**kw)
 
                 cur = con.cursor()
                 cur.escape_string = con.escape_string
                 if timezone:
-                    cur.execute("set time_zone=%s", (timezone, ))
+                    cur.execute("set time_zone=%s", (timezone,))
 
             except bottle.HTTPResponse as e:
                 raise bottle.HTTPError(500, "Database Error", e)
--- bottle_pymysql_test.py
+++ bottle_pymysql_test.py
@@ -1,18 +1,18 @@
 import unittest
 
 import bottle
-import bottle_pymysql
 import pymysql
-
 from pymysql import OperationalError
+
+import bottle_pymysql
 
 
 class BottlePyMySQLTest(unittest.TestCase):
-    USER = 'root'
-    PASS = ''
-    DBNAME = 'bottle_pymysql_test'
-    SOCKET = '/var/run/mysqld/mysqld.sock'
-    DBHOST = '127.0.0.1'
+    USER = "root"
+    PASS = ""
+    DBNAME = "bottle_pymysql_test"
+    SOCKET = "/var/run/mysqld/mysqld.sock"
+    DBHOST = "127.0.0.1"
 
     # copy from https://github.com/bottlepy/bottle-sqlite
     def test_with_keyword(self):
@@ -28,14 +28,14 @@ class BottlePyMySQLTest(unittest.TestCase):
         self._run(test_1)
 
         def test_2(**kw):
-            self.assertFalse('pymydb' in kw)
+            self.assertFalse("pymydb" in kw)
 
         self._run(test_2)
 
     def test_install_conflicts(self):
 
         app = self._app()  # install default
-        app = self._app(app=app, keyword='pymydb2')  # install another
+        app = self._app(app=app, keyword="pymydb2")  # install another
 
         def test(pymydb, pymydb2):
             self.assertEqual(type(pymydb), type(pymydb2))
@@ -44,9 +44,9 @@ class BottlePyMySQLTest(unittest.TestCase):
 
     def test_echo(self):
         def test(pymydb):
-            pymydb.execute(''' SELECT 1 AS TEST ''')
+            pymydb.execute(""" SELECT 1 AS TEST """)
 
-            self.assertEqual({'TEST': 1}, pymydb.fetchone())
+            self.assertEqual({"TEST": 1}, pymydb.fetchone())
 
         # test normal
         self._run(test, self._app(dbhost=self.DBHOST))
@@ -56,6 +56,7 @@ class BottlePyMySQLTest(unittest.TestCase):
 
     def _bad_connection_raise(self, app, word, test):
         try:
+
             def empty(pymydb):
                 pass
 
@@ -63,38 +64,34 @@ class BottlePyMySQLTest(unittest.TestCase):
 
         except OperationalError as e:
             # self.assertTrue(word in e.args[1])
-            #assert word in e.args[1]
+            # assert word in e.args[1]
             test(word, e)
             return
 
-        self.fail('should not success')
-
+        self.fail("should not success")
 
     def test_bad_connection_host(self):
-
         def test(word, e):
             assert word in e.args[1]
 
         # bad host
-        host = '255.255.255.255'
+        host = "255.255.255.255"
         self._bad_connection_raise(self._app(dbhost=host), host, test)
 
     def test_bad_connection_sock(self):
-
         def test(word, e):
-            assert hasattr(e, 'original_exception')
+            assert hasattr(e, "original_exception")
             oex = e.original_exception
             assert oex.errno == 2
 
         # bad sock
-        sock = '/not_exits.sock'
+        sock = "/not_exits.sock"
         self._bad_connection_raise(self._app(dbunixsocket=sock), sock, test)
 
     def test_dictrow(self):
-
         def equal_type(t):
             def test_type(pymydb):
-                pymydb.execute(''' SELECT 1 AS TEST ''')
+                pymydb.execute(""" SELECT 1 AS TEST """)
                 self.assertEqual(type(pymydb.fetchone()), t)
 
             return test_type
@@ -106,50 +103,64 @@ class BottlePyMySQLTest(unittest.TestCase):
         self._run(equal_type(tuple), app=self._app(dictrows=False))
 
     def test_timezone(self):
-
         def equal_tz(tz):
             def query_timezone(pymydb):
-                pymydb.execute(''' SELECT @@session.time_zone as TZ;''')
-                self.assertEqual({'TZ': tz}, pymydb.fetchone())
+                pymydb.execute(""" SELECT @@session.time_zone as TZ;""")
+                self.assertEqual({"TZ": tz}, pymydb.fetchone())
 
             return query_timezone
 
-        tz = '-08:00'
+        tz = "-08:00"
         self._run(equal_tz(tz), app=self._app(timezone=tz))
 
         # default tz
-        self._run(equal_tz('SYSTEM'))
+        self._run(equal_tz("SYSTEM"))
 
     def test_crud(self):
 
         self._create_test_table()
 
         def crud(pymydb):
-            data = 'test'
+            data = "test"
 
             # insert
-            rows = pymydb.execute(''' INSERT INTO `bottle_mysql_test` VALUE (NULL, %s) ''', (data,))
+            rows = pymydb.execute(
+                """ INSERT INTO `bottle_mysql_test` VALUE (NULL, %s) """,
+                (data,),
+            )
             self.assertEqual(rows, 1)
 
-            pymydb.execute(''' SELECT last_insert_id() as ID ''')
+            pymydb.execute(""" SELECT last_insert_id() as ID """)
 
-            data_id = pymydb.fetchone()['ID']
+            data_id = pymydb.fetchone()["ID"]
             self.assertTrue(data_id > 0)
 
             # select
-            pymydb.execute(''' SELECT * FROM `bottle_mysql_test` WHERE `id` = %s''', (data_id, ))
-            self.assertEqual({'id': data_id, 'text': data, }, pymydb.fetchone())
+            pymydb.execute(
+                """ SELECT * FROM `bottle_mysql_test` WHERE `id` = %s""",
+                (data_id,),
+            )
+            self.assertEqual({"id": data_id, "text": data}, pymydb.fetchone())
 
             # update
-            data = 'new'
-            rows = pymydb.execute(''' UPDATE `bottle_mysql_test` SET `text` = %s WHERE `id` = %s''', (data, data_id, ))
+            data = "new"
+            rows = pymydb.execute(
+                """ UPDATE `bottle_mysql_test` SET `text` = %s WHERE `id` = %s""",
+                (data, data_id),
+            )
             self.assertEqual(rows, 1)
 
-            pymydb.execute(''' SELECT * FROM `bottle_mysql_test` WHERE `id` = %s''', (data_id, ))
-            self.assertEqual({'id': data_id, 'text': data, }, pymydb.fetchone())
+            pymydb.execute(
+                """ SELECT * FROM `bottle_mysql_test` WHERE `id` = %s""",
+                (data_id,),
+            )
+            self.assertEqual({"id": data_id, "text": data}, pymydb.fetchone())
 
             # delete
-            rows = pymydb.execute(''' DELETE FROM `bottle_mysql_test` WHERE `id` = %s''', (data_id, ))
+            rows = pymydb.execute(
+                """ DELETE FROM `bottle_mysql_test` WHERE `id` = %s""",
+                (data_id,),
+            )
             self.assertEqual(rows, 1)
 
         self._run(crud)
@@ -167,11 +178,11 @@ class BottlePyMySQLTest(unittest.TestCase):
         app = self._app()
 
         # config with override
-        @app.get('/', pymysql={'autocommit': False})
+        @app.get("/", pymysql={"autocommit": False})
         def insert(pymydb):
             self._insert_one(pymydb)
 
-        self._request(app, '/')
+        self._request(app, "/")
         self.assert_records(0)
 
         # config with construct
@@ -183,7 +194,7 @@ class BottlePyMySQLTest(unittest.TestCase):
 
         def test(pymydb):
             self._insert_one(pymydb)
-            bottle.redirect('/')
+            bottle.redirect("/")
 
         self._run(test)
         self.assert_records(1)
@@ -202,36 +213,42 @@ class BottlePyMySQLTest(unittest.TestCase):
         self._create_test_table()
 
         def test(pymydb):
-            self._insert_one(pymydb, 'test')
+            self._insert_one(pymydb, "test")
             pymydb.execute(
-                "SELECT COUNT(*) AS c FROM `bottle_mysql_test` WHERE `text` LIKE '%%%s%%'" % pymydb.escape_string('te'))
-            self.assertEqual({'c': 1}, pymydb.fetchone())
+                "SELECT COUNT(*) AS c FROM `bottle_mysql_test` WHERE `text` LIKE '%%%s%%'"
+                % pymydb.escape_string("te")
+            )
+            self.assertEqual({"c": 1}, pymydb.fetchone())
 
         self._run(test)
         self.assert_records(1)
 
     def assert_records(self, count):
         def query_count(pymydb):
-            pymydb.execute('''SELECT COUNT(1) AS c FROM `bottle_mysql_test`''')
-            self.assertEqual({'c': count}, pymydb.fetchone())
+            pymydb.execute("""SELECT COUNT(1) AS c FROM `bottle_mysql_test`""")
+            self.assertEqual({"c": count}, pymydb.fetchone())
 
         self._run(query_count)
 
-    def _insert_one(self, pymydb, data='test'):
-        rows = pymydb.execute(''' INSERT INTO `bottle_mysql_test` VALUE (NULL, %s) ''', (data,))
+    def _insert_one(self, pymydb, data="test"):
+        rows = pymydb.execute(
+            """ INSERT INTO `bottle_mysql_test` VALUE (NULL, %s) """, (data,)
+        )
         self.assertEqual(rows, 1)
 
     def _create_test_table(self):
         def init(pymydb):
-            pymydb.execute('''DROP TABLE IF EXISTS `bottle_mysql_test`; ''')
+            pymydb.execute("""DROP TABLE IF EXISTS `bottle_mysql_test`; """)
 
-            pymydb.execute('''
+            pymydb.execute(
+                """
             CREATE TABLE `bottle_mysql_test` (
               `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
               `text` varchar(11) DEFAULT NULL,
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            ''')
+            """
+            )
 
         self._run(init)
 
@@ -240,25 +257,27 @@ class BottlePyMySQLTest(unittest.TestCase):
         if not app:
             app = self._app()
 
-        app.get('/')(f)
-        self._request(app, '/')
+        app.get("/")(f)
+        self._request(app, "/")
 
     def _app(self, **kwargs):
-        app = kwargs.pop('app', bottle.Bottle(catchall=False))
+        app = kwargs.pop("app", bottle.Bottle(catchall=False))
 
-        kwargs.setdefault('dbuser', self.USER)
-        kwargs.setdefault('dbpass', self.PASS)
-        kwargs.setdefault('dbname', self.DBNAME)
-        kwargs.setdefault('dbhost', self.DBHOST)
+        kwargs.setdefault("dbuser", self.USER)
+        kwargs.setdefault("dbpass", self.PASS)
+        kwargs.setdefault("dbname", self.DBNAME)
+        kwargs.setdefault("dbhost", self.DBHOST)
         plugin = bottle_pymysql.Plugin(**kwargs)
 
         app.install(plugin)
 
         return app
 
-    def _request(self, app, path, method='GET'):
-        return app({'PATH_INFO': path, 'REQUEST_METHOD': method}, lambda x, y: None)
+    def _request(self, app, path, method="GET"):
+        return app(
+            {"PATH_INFO": path, "REQUEST_METHOD": method}, lambda x, y: None
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
